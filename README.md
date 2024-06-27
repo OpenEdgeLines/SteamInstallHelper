79b545f7d81da4183352e38a408118df3c310524
rootber <122451668+rootber@users.noreply.github.com>
2024-06-27 19:10:56 +0800

    修改了一些不必要的部分 如单文件无需CMake 不必言的部分逻辑

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # SteamInstallHelper
 本项目没有任何技术含量有过其他语言开发经验的很快也能开发出来
-本项目依赖wget可执行文件
+本项目使用Invoke-WebRequest下载安装程序
 由于本人Linux平台使用WattToolKit存在问题为避免出现问题暂不提供Linux的支持
+# 编译
+因为只有一个源码文件所以直接用g++活msvc编译即可
+## g++
+````
+g++ SteamInstallHelper.cpp -o SteamInstallHelper
--- SteamInstallHelper.cpp
+++ SteamInstallHelper.cpp
@@ -1,12 +1,19 @@
 #include<iostream>
 #include<cstdlib>
+#include<windows.h>
 int main() 
 {
 	system("echo 欢迎使用Steam安装助手(名字跟盗版Steam有得一拼)");
 	system("echo 本项目的初衷是帮助赛博文盲安装Steam");
-	system("echo 为了省事直接用命令调用wget下载安装程序了QwQ");
-	system("wget https://github.com/BeyondDimension/SteamTools/releases/download/3.0.0-rc.8/Steam++_v3.0.0-rc.8_win_x64.exe");
-	system("wget https://media.st.dl.eccdnx.com/client/installer/SteamSetup.exe");
-	system("./Steam++_v3.0.0-rc.8_win_x64.exe");
-	system("./SteamSetup.exe");
+	system("echo 为了省事直接用命令调用Invoke-WebRequest下载安装程序了QwQ");
+	Sleep(10000);
+
+	system("mkdir DownloadFile");
+	system("Invoke-WebRequest -Uri https://github.com/BeyondDimension/SteamTools/releases/download/3.0.0-rc.8/Steam++_v3.0.0-rc.8_win_x64.exe -OutFile DownloadFile/Steam++.exe");
+	system("Invoke-WebRequest https://media.st.dl.eccdnx.com/client/installer/SteamSetup.exe -OutFile DownlaodFile/SteamSetup.exe");
+	system("DownloadFile/Steam++_v3.0.0-rc.8_win_x64.exe");
+	system("DownloadFile/SteamSetup.exe");
+	system("echo 安装程序即将退出");
+	Sleep(10000);
+
 }
